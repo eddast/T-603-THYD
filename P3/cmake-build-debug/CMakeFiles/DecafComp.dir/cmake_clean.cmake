37412d5968a27f155b0b06e33b3773f2f34ef1d2
eddast <eddasr15@ru.is>
2018-10-20 22:17:57 +0000

    && and || works

---
+++ P3/cmake-build-debug/CMakeFiles/DecafComp.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../parser_decaf.cpp"
+  "../parser_decaf.hpp"
+  "../lexer_decaf.cpp"
+  "CMakeFiles/DecafComp.dir/parser_decaf.cpp.o"
+  "CMakeFiles/DecafComp.dir/lexer_decaf.cpp.o"
+  "CMakeFiles/DecafComp.dir/main.cpp.o"
+  "DecafComp.pdb"
+  "DecafComp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DecafComp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
